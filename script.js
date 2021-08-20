0958d8d4926b6e9d0f7af1c271953d3121dc91d0
29Badde <71213857+29Badde@users.noreply.github.com>
2021-08-20 13:38:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function sendMail(params) {
     var tempParams = {
         from_name:document.getElementById("fromName").value,
         to_name:document.getElementById("toName").value,
-        message:document.getElementById("msg").value
+        message:document.getElementById("msg").value,
     };
 
-    emailjs.send('gmail','template_58cre3d','tempParams')
+    emailjs.send('gmail','template_58cre3d',tempParams)
     .then(function(res){
         console.log("success", res.status);
     })
